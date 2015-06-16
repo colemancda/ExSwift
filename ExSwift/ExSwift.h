875616ed59ff7d1517fa273e373ca82d11a74cd1
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-16 09:25:28 -0500

    made Universal OS X / iOS framework
    again

--- ExSwift/ExSwift.h
+++ ExSwift/ExSwift.h
@@ -14,6 +14,5 @@ FOUNDATION_EXPORT double ExSwiftVersionNumber;
 //! Project version string for ExSwift.
 FOUNDATION_EXPORT const unsigned char ExSwiftVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ExSwift/PublicHeader.h>
 
 
