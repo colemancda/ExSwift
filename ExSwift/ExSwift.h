b8a14e6e4ac5232241be52a30d92d56097c23f38
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-17 15:01:20 -0500

    Revert "made Universal OS X / iOS framework"
    This reverts commit 875616ed59ff7d1517fa273e373ca82d11a74cd1.

--- ExSwift/ExSwift.h
+++ ExSwift/ExSwift.h
@@ -14,5 +14,6 @@ FOUNDATION_EXPORT double ExSwiftVersionNumber;
 //! Project version string for ExSwift.
 FOUNDATION_EXPORT const unsigned char ExSwiftVersionString[];
 
+// In this header, you should import all the public headers of your framework using statements like #import <ExSwift/PublicHeader.h>
 
 
